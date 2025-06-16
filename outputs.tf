e54fbb09e21cd1cd908da9125b83967db3c3eaf0
Alex Reiff <alex.reiff@ibm.com>
2025-06-16 15:27:54 -0400

    remove outputes

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,12 @@
 # Outputs
 ########################################################################################################################
 
-output "name" {
-  description = "Name of created SCC WP instance."
-  value       = local.catalog_account_boundary
-}
+# output "name" {
+#   description = "Name of created SCC WP instance."
+#   value       = local.catalog_account_boundary
+# }
 
-output "name2" {
-  description = "Name of created SCC WP instance."
-  value       = local.data
-}
+# output "name2" {
+#   description = "Name of created SCC WP instance."
+#   value       = local.data
+# }
