b50578f222c4d1e24d9719ce0e0d81d2b6362b8e
Alex Reiff <alex.reiff@ibm.com>
2025-06-16 17:41:14 -0400

    test cloud

--- main.tf
+++ main.tf
@@ -11,43 +11,15 @@ locals {
   catalog_revision         = restapi_object.catalog_info.api_data["_rev"]
   catalog_account_boundary = restapi_object.catalog_info.api_data["account_boundary"]
   catalog_account_filters  = restapi_object.catalog_info.api_data["account_filters"]
-  # data = jsonencode({
-  #   id   = local.catalog_id
-  #   _rev = local.catalog_revision
-
-  #   account_boundary = local.catalog_account_boundary
-  #   account_filters  = local.catalog_account_filters
-  #   terraform_engines = [
-  #     {
-  #       name : var.terraform_enterprise_engine_name
-  #       type           = "tfe"
-  #       public_endoint = var.terraform_enterprise_hostname
-  #       api_token      = var.terraform_enterprise_instance_token
-  #       da_creation = {
-  #         enabled                    = var.enable_automatic_deployable_architecture_creation
-  #         default_private_catalog_id = var.default_private_catalog_id
-  #         # "polling_info": { // If polling info is not provided, we will try to auto-create DAs in all workspaces in all orgs
-  #         #   "scopes": [
-  #         #     {
-  #         #       "name": "kz-test",
-  #         #       "type": "project" // Type can be project | org | workspace to poll on to auto-create DAs
-  #         #     }
-  #         #   ]
-  #         # }
-  #       }
-  #     }
-  #   ]
-  # })
-}
-
-resource "restapi_object" "tfe-engines" { //need to PUT all fields in the body, TODO pull down whole object and update in place
-  path = "/api/v1-beta/catalogaccount"
-  data = jsonencode({
+  data = {
     id   = local.catalog_id
     _rev = local.catalog_revision
 
-    account_boundary = local.catalog_account_boundary
-    account_filters  = local.catalog_account_filters
+    # account_boundary = local.catalog_account_boundary
+    account_filters = {
+      include_all = true,
+      id_filters  = {}
+    }
     terraform_engines = [
       {
         name : var.terraform_enterprise_engine_name
@@ -55,8 +27,8 @@ resource "restapi_object" "tfe-engines" { //need to PUT all fields in the body, 
         public_endoint = var.terraform_enterprise_hostname
         api_token      = var.terraform_enterprise_instance_token
         da_creation = {
-          enabled                    = var.enable_automatic_deployable_architecture_creation
-          default_private_catalog_id = var.default_private_catalog_id
+          enabled = var.enable_automatic_deployable_architecture_creation
+          # default_private_catalog_id = var.default_private_catalog_id
           # "polling_info": { // If polling info is not provided, we will try to auto-create DAs in all workspaces in all orgs
           #   "scopes": [
           #     {
@@ -68,7 +40,12 @@ resource "restapi_object" "tfe-engines" { //need to PUT all fields in the body, 
         }
       }
     ]
-  })
+  }
+}
+
+resource "restapi_object" "tfe-engines" { //need to PUT all fields in the body, TODO pull down whole object and update in place
+  path           = "/api/v1-beta/catalogaccount"
+  data           = jsonencode(local.data)
   create_method  = "PUT" # Specify the HTTP method for updates
   update_method  = "PUT"
   destroy_method = "PUT"
